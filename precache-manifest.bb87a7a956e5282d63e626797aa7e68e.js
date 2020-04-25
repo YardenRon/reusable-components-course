427a537ada14af9de867624c3cd58909a5e7ed66
Yarden Ron <yardenron4@gmail.com>
2020-04-25 14:49:19 +0300

    Updates

---
+++ precache-manifest.bb87a7a956e5282d63e626797aa7e68e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e237f9081e5e830dfbe5e15eedf59260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85d15b37489041c1ba5e",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "b0ae03ff8e2dadd55d0b",
+    "url": "/static/css/main.675efae5.chunk.css"
+  },
+  {
+    "revision": "85d15b37489041c1ba5e",
+    "url": "/static/js/2.5910ad73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5910ad73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ae03ff8e2dadd55d0b",
+    "url": "/static/js/main.275e422e.chunk.js"
+  },
+  {
+    "revision": "145ba903c0410c2f8b51",
+    "url": "/static/js/runtime-main.fa78011c.js"
+  }
+]);
